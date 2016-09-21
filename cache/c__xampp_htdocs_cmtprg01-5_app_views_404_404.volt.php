11e39a1a6b5907df0e72d9999a94f0d7f0710662
SRPater <srpater@gmail.com>
2016-09-21 15:19:27 +0200

    Created custom 404 error page.

---
+++ cache/c__xampp_htdocs_cmtprg01-5_app_views_404_404.volt.php
@@ -0,0 +1,9 @@
+<div class="page-header">
+    <h1>Uh-oh!</h1>
+</div>
+
+<img id="fourohfour" src="img/404.png" />
+
+<p>This page does not seem to exist.</p>
+
+<a class="btn btn-primary" href="/cmtprg01-5" role="button">Return to home</a>
